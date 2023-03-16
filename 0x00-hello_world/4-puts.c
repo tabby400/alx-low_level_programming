49014261725975e1a6646c86ce92cb83da451ccc
tabby400 <tabbymargaret@gmail.com>
2023-03-16 10:24:20 -0700

    using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - shows main functions
+ *
+ * Return: ends with 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
