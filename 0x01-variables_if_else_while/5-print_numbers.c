32d6367f0dda51553e1ab9017199cde524ac54a7
tabby400 <tabbymargaret@gmail.com>
2023-03-17 11:26:15 -0700

    single digit numbers of base ten

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of function
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
