6d10de6754717f4ab37cee37f56201f73934f25d
tabby400 <tabbymargaret@gmail.com>
2023-03-17 15:02:04 -0700

    single digits ascending order

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of a function
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+
+
+}
