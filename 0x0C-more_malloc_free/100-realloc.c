cfe366eb93ff6743b1c2379d8caf4400930682c0
tabby400 <tabbynargaret@gmail.com>
2023-04-14 03:40:02 -0700

    reallocating new memory

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,23 +14,38 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	unsigned int p;
+	char *old;
+	char *new;
+
+
+	/*if ptr is NULL, malloc(new_size),is for  values of old_size and new_size*/
 	if (ptr == NULL)
 	{
 		ptr = malloc(new_size);
 	}
+	else
+		old = ptr;
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
-		return (NULL);
+		return (0);
 	}
-	/*if new size is greater added memory is not initialized if same return ptr*/
-	if (new_size == old_size)
+	new = malloc(new_size);
+	if (new == NULL)
 	{
-		return (ptr);
+		return (0);
 	}
-	/*free the space when necessary*/
-	free(ptr);
-	ptr = malloc(new_size);
+	if (new_size == old_size)
+		return (ptr);
+	/*if new_size is > old_size new memory shouldnt be initialized*/
+	for (p = 0 ; p < (new_size || p < old_size) ; p++)
+	{
+		new[p] = old[p];
 
-	return (ptr);
+	}
+	free(ptr);
+	return (new);
+
+
 }
