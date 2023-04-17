3bc59bd1a70c09b4ea37f1c505c8219ce3e71fcd
tabby400 <tabbynargaret@gmail.com>
2023-04-17 09:51:19 -0700

    new name for struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,7 +17,14 @@ struct dog
 	float age;
 }
 ;
+/**
+ * dog_t - new name of type struct dog
+ */
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 
 #endif
