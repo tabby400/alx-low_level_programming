ca6986d317300a5945845b15ae9bfebe84be76ff
tabby400 <tabbynargaret@gmail.com>
2023-04-27 10:36:48 -0700

    using attribute constructor

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,6 +10,6 @@ void __attribute__((constructor)) b4main(void);
 
 void b4main(void)
 {
-	printf("You're beat! and yet, you must allow\n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
