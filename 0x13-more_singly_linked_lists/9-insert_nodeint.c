e8f32ab508c24a3221ac95f1725298618413bb5a
tabby400 <tabbynargaret@gmail.com>
2023-04-30 05:06:08 -0700

    inserts new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	added = malloc(sizeof(listint_t));
 
-	if (added == NULL || head == NULL)
+	if (added == NULL && head == NULL)
 	{
 		return (NULL);/*if cannot be added*/
 	}
