7d2acf091ebce0e54278df5d4b8dabaeb02baf1a
tabby400 <tabbynargaret@gmail.com>
2023-05-08 10:46:34 -0700

    creating file and putting content in

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,12 +26,9 @@ int create_file(const char *filename, char *text_content)
 		for (p = 0 ; text_content[p] ;)
 			p++;
 	}
-	else
-	{
-		op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);/* file permissions*/
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);/* file permissions*/
+	wr = write(op, text_content, p);
 
-		wr = write(op, text_content, p);
-	}
 	if (op == -1 || wr == -1)
 	{
 		return (-1);
