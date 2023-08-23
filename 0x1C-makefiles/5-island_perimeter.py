4fd29dba20aed6d0e79fcdb3a48977f191333039
tabby400 <tabbynargaret@gmail.com>
2023-08-23 14:02:57 -0700

    perimeter island

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""definition of a function for island perimeter"""
+
+def island_perimeter(grid):
+    """ function that returns the perimeter of an island
+    Arguements:
+        grid: list of list of ints
+    Returns: perimeter of island
+    """
+
+
+
+    perimet = 0
+
+    for p in range(len(grid)):
+        for r in range(len(grid[p])):
+            if grid[p][r] == 1:
+                perimet = perimet + 4
+                if p > 0 and grid[p - 1][r] == 1:
+                    perimet -= 2
+                if r > 0 and grid[p][r - 1] == 1:
+                    perimetr -= 2
+    return perimet
